053def439f450c916113109cbf3747392e085d1a
bevanzpf <bevanzpf@gmail.com>
2016-05-09 12:23:01 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'static_pages/about'
+
+  get 'static_pages/home'
+
+  get 'users/new'
+
+  root 'static_pages#home'
+
+  get 'home'=> 'static_pages#home'
+
+  get 'about'=> 'static_pages#about'
+
+  get 'signup' => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
