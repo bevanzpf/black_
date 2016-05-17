0d6ea3d752afd02bd0eb5fc6828eb397e52dc9a9
bevanzpf <bevanzpf@gmail.com>
2016-05-17 10:39:42 +0800

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Example User",
+            email: "hello@gg.com",
+            password: "abc123",
+            password_confirmation: "abc123",
+            admin: true,
+            activated: true,
+            activated: Time.zone.now.to_datetime)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@black.com"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password: password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now.to_datetime)
+
+end
